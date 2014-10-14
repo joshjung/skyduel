2fcafc835d60f9fb2ea3e9e9483a16c6f0b9bfec
joshjung <joshua.p.jung@gmail.com>
2014-10-14 05:42:15 -0500

    Adding health bars.

--- web-server/public/js/reactjs/players.jsx
+++ web-server/public/js/reactjs/players.jsx
@@ -2,17 +2,21 @@
 var rjPlayers = React.createClass({
   componentDidMount: function () {
     this.setState({players: []});
-    setInterval(this.update, 500);  
+    setInterval(this.update, 100);  
   },
   render: function() {
     if (!this.state)
       return <div/>;
     var playerList = this.state.players.map(function (player) {
       var st = {color: '#' + player.colorHex, 'font-weight': 'bold'};
-      var phst = {'background-color': '#' + player.colorHex, width: Math.round(player.health/2) + 'px', height: '14px'};
+      var phst = {
+        'background-color': '#' + player.colorHex,
+        width: Math.round(player.health/2) + 'px',
+        height: '20px',
+        color: '#CCC'};
       var isYou = (window.client.uid == player.uid) ? '>' : '';
       var stat = player.health ? (<div className="player-health-wrapper">
-            <div style={phst}/>
+            <div style={phst}>{Math.round(player.health)}%</div>
           </div>) : (<span>DEAD </span>);
       return (<div style={st} className="player">
         <span className="player-name">{isYou}{player.username}</span>
